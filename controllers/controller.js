e4b18531372651ced1a4aaa1bcb9398af0408f6e
RamonJul <ramoncarlosjulian@gmail.com>
2019-03-27 10:37:44 -0400

    scrape is done

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,58 +1,60 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var router = express.Router();
-var axios = require("axios");
-var cheerio = require("cheerio");
-var db = require("./../models");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const axios = require("axios");
+const cheerio = require("cheerio");
+const db = require("./../models");
 
 mongoose.connect("mongodb://localhost/unit18Populater", {
   useNewUrlParser: true
 });
-router.get("/",function(req,res){
-  var results=[]
-    axios.get("https://www.cp24.com/world").then(function(response) {
 
-      
-        var $ = cheerio.load(response.data);
- 
-        $("li.dc").each(function(i, element) {
-          if(i<=10){
-            var link= $(element).children(".teaser").children(".teaser-image-wrapper").children(".teaserImage").children().attr("href");         
-            var image=$(element).children(".teaser").children(".teaser-image-wrapper").children(".teaserImage").children().children().attr("src");
-            var title=$(element).children(".teaser").children(".teaserText").children(".bn-headline").children().text().trim();
-            var text=$(element).children(".teaser").children(".teaserText").children(".teaserLead").children().text().trim();
+router.get("/", function (req, res) {
+  db.Article.find({}).then(function (data) {
 
-            var obj={
-                title:title,
-                link:link,
-                summary:text,
-                image:image
-        
-            }
-              results.push(obj);
-          }
-     
-        });
-      
-        console.log(results);
-       
-      });
-         res.json(results)
-})
-
-router.get("/articles",function(req,res){
-db.Article.find({}).then(function(data){
+  })
 
 })
 
+router.get("/scrape", (req, res) => {
+  axios.get("https://www.cp24.com/world").then((response) => {
+    const $ = cheerio.load(response.data);
+
+    $("li.dc").each(function (i, element) {
+
+      let link = $(element).children(".teaser").children(".teaser-image-wrapper").children(".teaserImage").children().attr("href");
+      let image = $(element).children(".teaser").children(".teaser-image-wrapper").children(".teaserImage").children().children().attr("src");
+      let title = $(element).children(".teaser").children(".teaserText").children(".bn-headline").children().text().trim();
+      let text = $(element).children(".teaser").children(".teaserText").children(".teaserLead").children().text().trim();
+
+      if (link && image && title && text) {
+        let obj = {
+          title: title,
+          link: link,
+          summary: text,
+          image: image
+
+        }
+        db.Article.create(obj).then((articles) => {
+          console.log(articles)
+        })
+    
+      }
+
+    });
+
+  });
+
+})
+//load the articles
+
+
+router.get("/articles./:id", function (req, res) {
+
+
 })
 
-router.get("/articles./:id",function(req,res){
-
-
-})
-
-router.post("/articles/:id",function(req,res){
+router.post("/articles/:id", function (req, res) {
 
 })
-module.exports=router;
+module.exports = router;
